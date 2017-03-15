f344944bc8918293ed2c35220ab8f06eb1d7fc14
Fabian Krueger <Fabian Krueger>
2017-03-15 01:40:57 +0100

    init Testapp

---
+++ src/pages/page1/page1.ts
@@ -0,0 +1,29 @@
+// import 'pdfjs-dist/build/pdf.js';
+// import 'pdfjs-dist/web/pdf_viewer.js';
+
+import { Component } from '@angular/core';
+
+import { NavController } from 'ionic-angular';
+
+
+@Component({
+  selector: 'page-page1',
+  templateUrl: 'page1.html'
+})
+export class Page1 {
+
+  constructor(public navCtrl: NavController) {
+    // var container = document.getElementById('pdfContainer');
+    // PDFJS.useOnlyCssZoom = true;
+    // PDFJS.disableTextLayer = true;
+    // PDFJS.maxImageSize = 1024 * 1024;
+    // PDFJS.workerSrc = '../../build/dist/build/pdf.worker.js';
+    // PDFJS.cMapUrl = '../../build/dist/cmaps/';
+    // PDFJS.cMapPacked = true;
+
+
+  }
+
+
+
+}
